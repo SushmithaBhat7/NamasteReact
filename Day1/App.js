a2b6b07de7cc3a0eba154b151470e5cede110e65
unknown <sushmitha.bhat7@gmail.com>
2023-08-29 07:13:39 +0530

    Day3 code updated

--- Day1/App.js
+++ Day1/App.js
@@ -10,17 +10,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parentElem = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 element"),
-    React.createElement("h2", {}, "I'm h2 element"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 element"),
-    React.createElement("h2", {}, "I'm h2 element"),
-  ]),
-]);
+// const parentElem = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm h1 element"),
+//     React.createElement("h2", {}, "I'm h2 element"),
+//   ]),
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm h1 element"),
+//     React.createElement("h2", {}, "I'm h2 element"),
+//   ]),
+// ]);
+
+const parentElem = (
+  <h1 id="parent" xyz="ASGFCJHVJHJKJKJHK" color="blue">
+    Namaste from JSX
+  </h1>
+);
+const MiniTitleComp = () => <h3>Namasthe</h3>;
+const Title = () => (
+  <div id="title">
+    <MiniTitleComp />
+    <h2>Namasthe Title</h2>
+  </div>
+);
+const HeadingComponent = () => {
+  return (
+    <div className="hedding">
+      <Title />
+      <h1>Namasthe from HeadingComponent</h1>
+    </div>
+  );
+};
 console.log({ parentElem });
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parentElem);
+root.render(<HeadingComponent />);
